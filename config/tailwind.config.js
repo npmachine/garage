cb8f6f4caa099ae8c6486ca54e1c87ec32b4a320
npmachine <engineered.design@gmail.com>
2024-03-27 09:31:45 +0900

    feat: add shadcn_ui

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,23 +1,50 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class"],
+  // content: ["app/**/*.{ts,tsx}", "components/**/*.{ts,tsx}"],
   content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    "./public/*.html",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.{ts,tsx,js,jsx}",
+    "./app/views/**/*.{erb,haml,html,slim}",
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      // fontFamily: {
+      //   sans: ["var(--font-sans)", ...defaultTheme.fontFamily.sans],
+      //   // sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      // },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
-  ]
+  plugins: [require("tailwindcss-animate")],
+  // plugins: [
+  //   require('@tailwindcss/forms'),
+  //   require('@tailwindcss/aspect-ratio'),
+  //   require('@tailwindcss/typography'),
+  //   require('@tailwindcss/container-queries'),
+  // ],
 }
