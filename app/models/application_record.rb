030cacfeeddfd46dc5d564e5d250f7c41fbc554d
npmachine <engineered.design@gmail.com>
2024-03-28 14:00:50 +0900

    feat: remove unused files

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,4 +2,8 @@
 
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  def as_json(options = {})
+    super(options).transform_keys { |key| key.to_s.camelize(:lower) }
+  end
 end
