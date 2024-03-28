1e8b4b1809990e9e585081e60df40f2994d67b58
npmachine <engineered.design@gmail.com>
2024-03-29 00:13:55 +0900

    feat: add signup/signin page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,19 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :configure_devise_permitted_parameters, if: :devise_controller?
+  layout "layouts/full_body", if: :devise_controller?
+
   def decoded_param
     UrlEncoder.decode Array(params[:id].to_i)
   end
+
+  private
+    def configure_devise_permitted_parameters
+      devise_params = %i[name email password password_confirmation]
+
+      devise_parameter_sanitizer.permit(:sign_up, keys: devise_params)
+      devise_parameter_sanitizer.permit(:sign_in, keys: devise_params << :login)
+      devise_parameter_sanitizer.permit(:account_update, keys: devise_params << :current_password)
+    end
 end
