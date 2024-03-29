66513b92e3cf7ba7a20121c8b5ddc42638803267
npmachine <engineered.design@gmail.com>
2024-03-29 15:04:23 +0900

    feat: update tailwind env

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
 
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./public/*.html",
@@ -8,45 +8,37 @@ module.exports = {
     "./app/javascript/**/*.{ts,tsx,js,jsx}",
     "./app/views/**/*.{erb,haml,html,slim}",
   ],
-  // darkMode: ["class"],
-  // prefix: "",
   theme: {
-    container: {
-      center: true,
-      padding: "2rem",
-      screens: {
-        "2xl": "1400px",
-      },
-    },
     extend: {
       fontFamily: {
-        // sans: ["var(--font-sans)", ...defaultTheme.fontFamily.sans],
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
+      colors: {
+        primary: colors.teal,
+        secondary: colors.emerald,
+        danger: colors.red,
       },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
+      // keyframes: {
+      //   "accordion-down": {
+      //     from: { height: "0" },
+      //     to: { height: "var(--radix-accordion-content-height)" },
+      //   },
+      //   "accordion-up": {
+      //     from: { height: "var(--radix-accordion-content-height)" },
+      //     to: { height: "0" },
+      //   },
+      // },
+      // animation: {
+      //   "accordion-down": "accordion-down 0.2s ease-out",
+      //   "accordion-up": "accordion-up 0.2s ease-out",
+      // },
     },
   },
   plugins: [
     require("tailwindcss-animate"),
-    // require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/container-queries'),
   ],
-  // plugins: [
-  //   require('@tailwindcss/forms'),
-  //   require('@tailwindcss/aspect-ratio'),
-  //   require('@tailwindcss/typography'),
-  //   require('@tailwindcss/container-queries'),
-  // ],
 }
