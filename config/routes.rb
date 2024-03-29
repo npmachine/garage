9c646ab14d2b1fc0e0ca348e5cad56efaeff5374
npmachine <engineered.design@gmail.com>
2024-03-29 22:43:08 +0900

    fix: devise registration flash issue

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,9 @@ Rails.application.routes.draw do
 
   root "home#index"
 
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: "users/registrations"
+  }
 
   resources :posts, except: [:delete]
 end
