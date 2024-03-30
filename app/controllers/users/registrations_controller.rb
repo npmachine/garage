7747cf2fb875566f94f8c76af06285b1ff1ca7f0
npmachine <engineered.design@gmail.com>
2024-03-30 12:12:59 +0900

    feat: remove unused prints

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -11,20 +11,16 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # POST /resource
   def create
-    puts "Users::RegistrationsController#create"
     build_resource(sign_up_params)
-    puts "resource: #{resource.inspect}"
     resource.save
-    puts "resource.persisted?: #{resource.persisted?}"
+
     yield resource if block_given?
     if resource.persisted?
-      puts "resource.active_for_authentication?: #{resource.active_for_authentication?}"
       if resource.active_for_authentication?
         set_flash_message! :notice, :signed_up
         sign_up(resource_name, resource)
         respond_with resource, location: after_sign_up_path_for(resource)
       else
-        puts "resource.inactive_message: #{resource.inactive_message}"
         set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
         expire_data_after_sign_in!
         respond_with resource, location: after_inactive_sign_up_path_for(resource)
