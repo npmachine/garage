49c7b9ea00f1b82680f84ed96510cc132067700d
npmachine <engineered.design@gmail.com>
2024-03-31 11:42:04 +0900

    feat: store user location

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  # The callback which stores the current location must be added before you authenticate the user
+  # as `authenticate_user!` (or whatever your resource is) will halt the filter chain and redirect
+  # before the location can be stored.
+  before_action :store_user_location!, if: :storable_location?
   before_action :configure_devise_permitted_parameters, if: :devise_controller?
 
   def decoded_param
@@ -14,5 +18,18 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.permit(:sign_up, keys: devise_params)
       devise_parameter_sanitizer.permit(:sign_in, keys: devise_params << :login)
       devise_parameter_sanitizer.permit(:account_update, keys: devise_params << :current_password)
+    end
+
+    # Its important that the location is NOT stored if:
+    # - The request method is not GET (non idempotent)
+    # - The request is handled by a Devise controller such as Devise::SessionsController as that could cause an infinite redirect loop.
+    # - The request is an Ajax request as this can lead to very unexpected behaviour.
+    def storable_location?
+      request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+    end
+
+    def store_user_location!
+      # :user is the scope we are authenticating
+      store_location_for(:user, request.fullpath)
     end
 end
